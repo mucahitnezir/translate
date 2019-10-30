982be5203fafb7d524704f198c7adc437679748b
Mücahit Nezir <mucahitnezir@gmail.com>
2019-10-30 17:46:25 +0300

    Environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase';
 
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyCrCAWGVLQQusq3nr6WzEKabwWttmmx-_8',
-  authDomain: 'medixo-translate.firebaseapp.com',
-  databaseURL: 'https://medixo-translate.firebaseio.com',
-  projectId: 'medixo-translate',
-  storageBucket: 'medixo-translate.appspot.com',
-  messagingSenderId: '150309961932',
-  appId: '1:150309961932:web:239f53917b4867f71c8269',
-  measurementId: 'G-M01DWQDZF3',
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
 });
 
 export const auth = app.auth();
--- src/services/aws.js
+++ src/services/aws.js
@@ -1,9 +1,9 @@
 import * as AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: 'AKIAVE7SOPOQNH5D62XC',
-  secretAccessKey: '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',
-  region: 'eu-central-1',
+  accessKeyId: process.env.VUE_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.VUE_APP_AWS_SECRET_ACCESS_KEY,
+  region: process.env.VUE_APP_AWS_REGION,
 });
 
 export const translate = new AWS.Translate({ apiVersion: '2017-07-01' });
