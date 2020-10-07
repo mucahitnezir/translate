2807fc4b69add884a02dc6eb4b42d833ae016266
Mücahit Nezir <mucahitnezir@outlook.com.tr>
2020-10-07 23:40:41 +0300

    [client] profile edit view

--- client/src/views/Profile/Edit.vue
+++ client/src/views/Profile/Edit.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
-    <UpdateProfileForm class="mb-2" />
-    <v-row>
-      <v-col cols="12" md="6">
-        <UpdateEmailForm />
-      </v-col>
-      <v-col cols="12" md="6">
-        <UpdatePasswordForm />
-      </v-col>
-    </v-row>
-  </div>
+  <v-row>
+    <v-col cols="12" class="pt-0">
+      <UpdateProfileForm />
+    </v-col>
+    <v-col cols="12" md="6">
+      <UpdateEmailForm />
+    </v-col>
+    <v-col cols="12" md="6">
+      <UpdatePasswordForm />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import UpdateProfileForm from '@/components/Profile/UpdateProfileForm.vue';
 import UpdateEmailForm from '@/components/Profile/UpdateEmailForm.vue';
+import UpdateProfileForm from '@/components/Profile/UpdateProfileForm.vue';
 import UpdatePasswordForm from '@/components/Profile/UpdatePasswordForm.vue';
 
 export default {
